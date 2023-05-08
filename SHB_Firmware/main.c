454c8368e804f8461236f1a3005c962582856a0d
tm6473nb <nico@bieberich.info>
2023-05-08 20:05:19 +0200

    Init Project

---
+++ SHB_Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * SHB_Firmware.c
+ *
+ * Created: 08.05.2023 20:00:36
+ * Author : nico
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
