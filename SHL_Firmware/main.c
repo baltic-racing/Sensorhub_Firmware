4640e1b419535d9c74af410d7488e7c9e4029d0f
tm6473nb <nico@bieberich.info>
2023-05-08 20:06:11 +0200

    Init Project

---
+++ SHL_Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * SHL_Firmware.c
+ *
+ * Created: 08.05.2023 20:03:09
+ * Author : nico
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
