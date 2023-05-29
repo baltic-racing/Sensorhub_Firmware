b4d89380ec426e6e8ea23f7e19936517c17d37d3
adam123467 <adaam1157@gmail.com>
2023-05-29 13:53:23 +0200

    addin CAN to SHR main

--- SHR_Firmware/main.c
+++ SHR_Firmware/main.c
@@ -7,21 +7,55 @@
 
 /*	INCLUDES	*/
 
-#include <avr/io.h>
+#include <avr/io.h>er
 #include "system_config.h"
+#include "adc_functions.h"
+#include "canlib.h"
 
 /*	Init Global Variables	*/
 unsigned long sys_tick = 0;
+unsigned long time_old = 0;
+unsigned long time_100_ms = 0;
 
 int main(void)
 {
 	/*	Configuration	*/
 	port_config();
 	sys_timer_config();
+	adc_config(); //CHECK WHICH ADC INPUTS CORRESPOND WITH WHAT DATABYTE WE SEND
+	can_cfg();
+	
+	struct CAN_MOB can_SHR_mob;
+	can_SHR_mob.mob_id = 0x400;
+	can_SHR_mob.mob_idmask = 0x0; //send
+	can_SHR_mob.mob_number = 2;
+	uint8_t SHR_databytes[8];
+	
 	
     while (1) 
     {
-		
+		if((sys_tick - time_old) >= 1){
+			time_old = sys_tick;
+			adc_start_conversion();
+			time_100_ms++;
+
+			
+		}
+		if (time_100_ms <= 100){
+			PORTC ^= (1<<PC2);
+			
+			SHR_databytes[0] = adc_get(1)		& 0xff	; //lsb BPS1
+			SHR_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb BPS1
+			SHR_databytes[2] = adc_get(2)		& 0xff	; //lsb BPS2
+			SHR_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb BPS2
+			SHR_databytes[4] = 0; //SPI getter Wheel Speed R lsb
+			SHR_databytes[5] = 0; //SPI getter Wheel Speed R msb
+			SHR_databytes[6] = 0;
+			SHR_databytes[7] = 0;
+			
+			can_tx(&can_SHR_mob, SHR_databytes);
+			time_100_ms = 0;
+		} //no fault condition
     }
 }
 
