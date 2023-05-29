320849506334eec8abf9bfd3b20bc87aeccebf16
adam123467 <adaam1157@gmail.com>
2023-05-29 15:15:14 +0200

    matches DIC code now

--- SHB_Firmware/main.c
+++ SHB_Firmware/main.c
@@ -24,11 +24,12 @@ int main(void){
 	can_cfg();
 	adc_config();							//CAN lib may be outdated, small differences between the old can lib but i already used it on FB to communicate between the old SWC during initial testing 
 								//for message objects, the sensor only SENDS data!
-	struct CAN_MOB can_SHB_mob;
-	can_SHB_mob.mob_id = 0x420;
-	can_SHB_mob.mob_idmask = 0; //send
-	can_SHB_mob.mob_number = 0; //only 1 object here, do i need to adjust numbers across all software connected with CAN?
-	uint8_t SHB_databytes[8];
+	struct CAN_MOB can_SHB0_mob;
+	can_SHB0_mob.mob_id = 0x420;
+	can_SHB0_mob.mob_idmask = 0; //receive with no filter?
+	can_SHB0_mob.mob_number = 5;
+	uint8_t SHB0_databytes[8];
+	
 	
 	sei();
 	
@@ -42,16 +43,16 @@ int main(void){
 		}
 		if (time_20_ms <= 20){ 
 			
-			SHB_databytes[0] = adc_get(1)		& 0xff	; //lsb 
-			SHB_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb
-			SHB_databytes[2] = adc_get(2)		& 0xff	; //lsb 
-			SHB_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb
-			SHB_databytes[4] = adc_get(2)		& 0xff	; //lsb 
-			SHB_databytes[5] = (adc_get(2)>>8)	& 0xff	; //msb
-			SHB_databytes[6] = adc_get(3)		& 0xff	; //lsb 
-			SHB_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb
+			SHB0_databytes[0] = adc_get(1)		& 0xff	; //lsb 
+			SHB0_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb
+			SHB0_databytes[2] = adc_get(2)		& 0xff	; //lsb 
+			SHB0_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb
+			SHB0_databytes[4] = adc_get(2)		& 0xff	; //lsb 
+			SHB0_databytes[5] = (adc_get(2)>>8)	& 0xff	; //msb
+			SHB0_databytes[6] = adc_get(3)		& 0xff	; //lsb 
+			SHB0_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb
 			
-			can_tx(&can_SHB_mob, SHB_databytes);
+			can_tx(&can_SHB0_mob, SHB0_databytes);
 			time_20_ms = 0;
 		}
 			
