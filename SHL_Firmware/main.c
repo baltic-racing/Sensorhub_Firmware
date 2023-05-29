e7c568973f2115d3aaa86a7b405ba55ae5896464
adam123467 <adaam1157@gmail.com>
2023-05-29 15:14:55 +0200

    matches DIC code now

--- SHL_Firmware/main.c
+++ SHL_Firmware/main.c
@@ -24,12 +24,11 @@ int main(void){
 	adc_config(); //CHECK WHICH ADC INPUTS CORRESPOND WITH WHAT DATABYTE WE SEND
 	can_cfg();
 	
-	struct CAN_MOB can_SHL_mob;
-	can_SHL_mob.mob_id = 0x410;
-	can_SHL_mob.mob_idmask = 0x0; //send 
-	can_SHL_mob.mob_number = 1;
-	uint8_t SHL_databytes[8];
-	
+	struct CAN_MOB can_SHL0_mob;
+	can_SHL0_mob.mob_id = 0x410;
+	can_SHL0_mob.mob_idmask = 0;//sent
+	can_SHL0_mob.mob_number = 4;
+	uint8_t SHL0_databytes[8];
 	
 	
 	while (1){
@@ -43,16 +42,16 @@ int main(void){
 		if (time_100_ms <= 100){
 		sys_tick_heart();	
 			
-		SHL_databytes[0] = adc_get(1)		& 0xff	; //lsb BPS1
-		SHL_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb BPS1
-		SHL_databytes[2] = adc_get(2)		& 0xff	; //lsb BPS2
-		SHL_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb BPS2
-		SHL_databytes[4] = 0; //SPI getter Wheel Speed lsb
-		SHL_databytes[5] = 0; //SPI getter Wheel Speed msb
-		SHL_databytes[6] = adc_get(3)		& 0xff	; //lsb SA
-		SHL_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb SA
+		SHL0_databytes[0] = adc_get(1)		& 0xff	; //lsb BPS1
+		SHL0_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb BPS1
+		SHL0_databytes[2] = adc_get(2)		& 0xff	; //lsb BPS2
+		SHL0_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb BPS2
+		SHL0_databytes[4] = 0; //SPI getter Wheel Speed lsb
+		SHL0_databytes[5] = 0; //SPI getter Wheel Speed msb
+		SHL0_databytes[6] = adc_get(3)		& 0xff	; //lsb SA
+		SHL0_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb SA
 		
-		can_tx(&can_SHL_mob, SHL_databytes);
+		can_tx(&can_SHL0_mob, SHL0_databytes);
 		time_100_ms = 0;
 		}
 	} //no fault condition
