5c83f484c45b95040a65b33f7ceb2b4ee35469e4
adam123467 <adaam1157@gmail.com>
2023-05-29 15:13:52 +0200

    matches DIC code now

--- SHR_Firmware/main.c
+++ SHR_Firmware/main.c
@@ -7,7 +7,7 @@
 
 /*	INCLUDES	*/
 
-#include <avr/io.h>er
+#include <avr/io.h>
 #include "system_config.h"
 #include "adc_functions.h"
 #include "canlib.h"
@@ -25,11 +25,11 @@ int main(void)
 	adc_config(); //CHECK WHICH ADC INPUTS CORRESPOND WITH WHAT DATABYTE WE SEND
 	can_cfg();
 	
-	struct CAN_MOB can_SHR_mob;
-	can_SHR_mob.mob_id = 0x400;
-	can_SHR_mob.mob_idmask = 0x0; //send
-	can_SHR_mob.mob_number = 2;
-	uint8_t SHR_databytes[8];
+	struct CAN_MOB can_SHR0_mob;
+	can_SHR0_mob.mob_id = 0x400;
+	can_SHR0_mob.mob_idmask = 0; //send
+	can_SHR0_mob.mob_number = 3;  //IDs might be wrong
+	uint8_t SHR0_databytes[8];
 	
 	
     while (1) 
@@ -44,16 +44,16 @@ int main(void)
 		if (time_100_ms <= 100){
 			PORTC ^= (1<<PC2);
 			
-			SHR_databytes[0] = adc_get(1)		& 0xff	; //lsb APPS1
-			SHR_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb APPS1
-			SHR_databytes[2] = adc_get(2)		& 0xff	; //lsb APPS2
-			SHR_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb APPS2
-			SHR_databytes[4] = 0; //SPI getter Wheel Speed R lsb
-			SHR_databytes[5] = 0; //SPI getter Wheel Speed R msb
-			SHR_databytes[6] = 0;
-			SHR_databytes[7] = 0;
+			SHR0_databytes[0] = adc_get(1)		& 0xff	; //lsb APPS1
+			SHR0_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb APPS1
+			SHR0_databytes[2] = adc_get(2)		& 0xff	; //lsb APPS2
+			SHR0_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb APPS2
+			SHR0_databytes[4] = 0; //SPI getter Wheel Speed R lsb
+			SHR0_databytes[5] = 0; //SPI getter Wheel Speed R msb
+			SHR0_databytes[6] = 0;
+			SHR0_databytes[7] = 0;
 			
-			can_tx(&can_SHR_mob, SHR_databytes);
+			can_tx(&can_SHR0_mob, SHR0_databytes);
 			time_100_ms = 0;
 		} //no fault condition
     }
