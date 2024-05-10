33606dd9f3676ab8010f3f07978f6ff1bb0182bc
Egquus <marek.hagen@hochschule-stralsund.de>
2024-05-10 19:07:45 +0200

    Changes on the Heart-LED and Frequency
    Fixed an Issue that the Heart-LED do not Work
    Switch the CAN-Frequency to 10 Hz
    (but still in progress)

--- SHL_Firmware/main.c
+++ SHL_Firmware/main.c
@@ -16,7 +16,9 @@
 /*	Init Global Variables	*/
 unsigned long sys_tick = 0;
 unsigned long time_old = 0;
+unsigned long time_10_ms = 0;
 unsigned long time_100_ms = 0;
+
 int main(void){
 	/*	Configuration	*/
 	port_config();
@@ -27,31 +29,40 @@ int main(void){
 	struct CAN_MOB can_SHL0_mob;
 	can_SHL0_mob.mob_id = 0x410;
 	can_SHL0_mob.mob_idmask = 0;//sent
-	can_SHL0_mob.mob_number = 4;
-	uint8_t SHL0_databytes[8];
+	can_SHL0_mob.mob_number = 0;
+	uint8_t SHL0_databytes[8] = {0};
 	
+	sei();
 	
 	while (1){
 		if((sys_tick - time_old) >= 1){ 
 			time_old = sys_tick;
 			adc_start_conversion();
+			time_10_ms++;
 			time_100_ms++;  
 
 			
 		}
-		if (time_100_ms <= 100){
-		sys_tick_heart();	
+		if (time_10_ms >= 9)
+		{
+			SHL0_databytes[0] = adc_get(1)		& 0xff	; //lsb BPS1
+			SHL0_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb BPS1
+			SHL0_databytes[2] = adc_get(2)		& 0xff	; //lsb BPS2
+			SHL0_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb BPS2
+			SHL0_databytes[4] = 0; //SPI getter Wheel Speed lsb
+			SHL0_databytes[5] = 0; //SPI getter Wheel Speed msb
+			SHL0_databytes[6] = adc_get(3)		& 0xff	; //lsb SA
+			SHL0_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb SA
 			
-		SHL0_databytes[0] = adc_get(1)		& 0xff	; //lsb BPS1
-		SHL0_databytes[1] = (adc_get(1)>>8)	& 0xff	; //msb BPS1
-		SHL0_databytes[2] = adc_get(2)		& 0xff	; //lsb BPS2
-		SHL0_databytes[3] = (adc_get(2)>>8)	& 0xff	; //msb BPS2
-		SHL0_databytes[4] = 0; //SPI getter Wheel Speed lsb
-		SHL0_databytes[5] = 0; //SPI getter Wheel Speed msb
-		SHL0_databytes[6] = adc_get(3)		& 0xff	; //lsb SA
-		SHL0_databytes[7] = (adc_get(3)>>8)	& 0xff	; //msb SA
+			can_tx(&can_SHL0_mob, SHL0_databytes);
+			
+			time_10_ms = 0;
+		}
+		if (time_100_ms >= 100)
+		{
 		
-		can_tx(&can_SHL0_mob, SHL0_databytes);
+		PORTC ^= (1<<PC2);
+		
 		time_100_ms = 0;
 		}
 	} //no fault condition
