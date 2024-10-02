ebd6126acd3de30efc5002e879fca0168e6b88ed
AaronWeinstrauch <94782248+AaronWeinstrauch@users.noreply.github.com>
2024-10-02 15:22:24 +0200

    new project for ATmega16

---
+++ SHB_Firmware/SHB/ATMega16/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ * Created: 10/1/2024 6:58:07 PM
+ *  Author: aaron
+ */ 
+
+#include <xc.h>
+#include <avr/io.h>
+
+// global variables
+
+int main(void)
+{
+	//Configuration
+	
+	
+	
+    while(1)
+    {
+        
+    }
+}
---
+++ SHB_Firmware/SHB/ATMega16/system_config.c
@@ -0,0 +1,6 @@
+/*
+ * port_config.c
+ *
+ * Created: 01.10.2024 19:01:15
+ *  Author: aaron
+ */ 
