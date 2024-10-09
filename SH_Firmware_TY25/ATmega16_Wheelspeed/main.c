77e8084aa0c74ab503b5c18c9d076959cfbb7484
Valborian <62020204+Valborian@users.noreply.github.com>
2024-10-09 19:02:01 +0200

    ATmega16 wheelspeed

--- SH_Firmware_TY25/ATmega16_Wheelspeed/main.c
+++ SH_Firmware_TY25/ATmega16_Wheelspeed/main.c
@@ -1,18 +1,23 @@
 /*
- * SHR_Firmware_TY25.c
+ * ATmega16_Wheelspeed.c
  *
- * Created: 09.10.2024 17:02:35
+ * Created: 09.10.2024 18:21:32
  * Author : racin
  */ 
 
 #include <avr/io.h>
 
+#include "SPI.h"
+
 
 int main(void)
 {
+	SPI_SlaveInit();
+	uint8_t data = 0;
     /* Replace with your application code */
     while (1) 
     {
+		data = SPI_SlaveReceive();
     }
 }
 
